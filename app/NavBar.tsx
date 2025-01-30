fb7ba802e62ce35670a950b2fb8cd9e8878d4f41
HLockward <HLockward12@gmail.com>
2025-01-30 16:50:17 -0400

    Added style to the active link in the navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,14 @@
+"use client";
+
+import classnames from "classnames";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { BiTaskX } from "react-icons/bi";
 
 const NavBar = () => {
+  const pathName = usePathname();
+
   const links = [
     { name: "Dashboard", href: "/" },
     { name: "Tasks", href: "/tasks" },
@@ -18,7 +24,11 @@ const NavBar = () => {
           <Link
             key={link.href}
             href={link.href}
-            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+            className={classnames({
+              "text-zinc-900": pathName === link.href,
+              "text-zinc-500": pathName !== link.href,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
           >
             {link.name}
           </Link>
