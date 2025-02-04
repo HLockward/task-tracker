5f4d658c07ba3bdde03458eb4d45ef2ad7a6de44
HLockward <HLockward12@gmail.com>
2025-02-04 18:09:03 -0400

    Set up Radix UI

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Button } from "@radix-ui/themes";
 
 const Tasks = () => {
-  return <div>Tasks Page</div>;
+  return (
+    <div>
+      <Button>Create Task</Button>
+    </div>
+  );
 };
 
 export default Tasks;
