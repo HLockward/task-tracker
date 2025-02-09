39e6e4f33b8505abb00c2a5a0e3090151d0a6486
HLockward <HLockward12@gmail.com>
2025-02-09 18:00:05 -0400

    Implement client-side validation

--- app/tasks/new/page.tsx
+++ app/tasks/new/page.tsx
@@ -1,9 +1,12 @@
 "use client";
-import { TextField, Button, Callout } from "@radix-ui/themes";
+import { TextField, Button, Callout, Text } from "@radix-ui/themes";
 import { Controller, useForm } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createTaskSchema } from "@/app/validationSchemas";
+import { z } from "zod";
 //import SimpleMDE from "react-simplemde-editor";
 // We need to import like this in cases where components are not compatible with server-side rendering.
 import dynamic from "next/dynamic";
@@ -12,15 +15,19 @@ const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
 });
 import "easymde/dist/easymde.min.css";
 
-interface TaskForm {
-  title: string;
-  description: string;
-}
+type TaskForm = z.infer<typeof createTaskSchema>;
 
 const NewTaskPage = () => {
   const router = useRouter();
   const [error, setError] = useState("");
-  const { register, control, handleSubmit } = useForm<TaskForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TaskForm>({
+    resolver: zodResolver(createTaskSchema),
+  });
 
   return (
     <div className="max-w-xl">
@@ -41,6 +48,11 @@ const NewTaskPage = () => {
         })}
       >
         <TextField.Root placeholder="Title" {...register("title")} />
+        {errors.title && (
+          <Text color="red" as="p">
+            {errors.title.message}
+          </Text>
+        )}
         <Controller
           name="description"
           control={control}
@@ -48,6 +60,11 @@ const NewTaskPage = () => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
+        {errors.description && (
+          <Text color="red" as="p">
+            {errors.description.message}
+          </Text>
+        )}
 
         <Button>Submit New Task</Button>
       </form>
