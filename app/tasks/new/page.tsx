629d6fc0ffde9842d5f4e1d384dc479b446a9817
HLockward <HLockward12@gmail.com>
2025-02-09 17:21:53 -0400

    Handle errors

--- app/tasks/new/page.tsx
+++ app/tasks/new/page.tsx
@@ -1,8 +1,9 @@
 "use client";
-import { TextField, Button } from "@radix-ui/themes";
+import { TextField, Button, Callout } from "@radix-ui/themes";
 import { Controller, useForm } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 //import SimpleMDE from "react-simplemde-editor";
 // We need to import like this in cases where components are not compatible with server-side rendering.
 import dynamic from "next/dynamic";
@@ -18,27 +19,39 @@ interface TaskForm {
 
 const NewTaskPage = () => {
   const router = useRouter();
+  const [error, setError] = useState("");
   const { register, control, handleSubmit } = useForm<TaskForm>();
 
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/tasks", data);
-        router.push("/tasks");
-      })}
-    >
-      <TextField.Root placeholder="Title" {...register("title")} />
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description" {...field} />
-        )}
-      />
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-3">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/tasks", data);
+            router.push("/tasks");
+          } catch (error) {
+            setError("An error occurred. Please try again later.");
+          }
+        })}
+      >
+        <TextField.Root placeholder="Title" {...register("title")} />
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
 
-      <Button>Submit New Task</Button>
-    </form>
+        <Button>Submit New Task</Button>
+      </form>
+    </div>
   );
 };
 
