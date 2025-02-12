eb0b006633657075e5fa7f759e56ce4e714b4c07
HLockward <HLockward12@gmail.com>
2025-02-12 18:55:54 -0400

    Added the edit button

--- app/tasks/[id]/page.tsx
+++ app/tasks/[id]/page.tsx
@@ -1,8 +1,10 @@
 import { TaskStatusBadge } from "@/app/components";
 import { prisma } from "@/lib/prisma";
-import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import ReactMarkdown from "react-markdown";
+import { Pencil2Icon } from "@radix-ui/react-icons";
 
 interface Props {
   params: { id: string };
@@ -17,16 +19,24 @@ const TaskDetailPage = async ({ params }: Props) => {
   if (!task) notFound();
 
   return (
-    <div>
-      <Heading>{task.title}</Heading>
-      <Flex gap="3" my="2">
-        <TaskStatusBadge status={task.status} />
-        <Text>{task.createdAt.toDateString()}</Text>
-      </Flex>
-      <Card className="prose" mt="4">
-        <ReactMarkdown>{task.description}</ReactMarkdown>
-      </Card>
-    </div>
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Box>
+        <Heading>{task.title}</Heading>
+        <Flex gap="3" my="2">
+          <TaskStatusBadge status={task.status} />
+          <Text>{task.createdAt.toDateString()}</Text>
+        </Flex>
+        <Card className="prose" mt="4">
+          <ReactMarkdown>{task.description}</ReactMarkdown>
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <Pencil2Icon />
+          <Link href={`/tasks/${task.id}/edit`}>Edit Task</Link>
+        </Button>
+      </Box>
+    </Grid>
   );
 };
 
