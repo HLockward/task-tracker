743947711745c6290d18986c1179e6ac3ae5f991
HLockward <HLockward12@gmail.com>
2025-02-14 21:19:42 -0400

    Handle errors

--- app/tasks/[id]/(id)/DeleteTaskButton.tsx
+++ app/tasks/[id]/(id)/DeleteTaskButton.tsx
@@ -2,42 +2,69 @@
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const DeleteTaskButton = ({ taskId }: { taskId: number }) => {
   const router = useRouter();
-  return (
-    <AlertDialog.Root>
-      <AlertDialog.Trigger>
-        <Button color="red">Delete Task</Button>
-      </AlertDialog.Trigger>
-      <AlertDialog.Content maxWidth="450px">
-        <AlertDialog.Title>Confirm deletion</AlertDialog.Title>
-        <AlertDialog.Description size="2">
-          Are you sure you want to delete this task? This action cannot be
-          undone .
-        </AlertDialog.Description>
+  const [error, setError] = useState(false);
 
-        <Flex gap="3" mt="4" justify="end">
-          <AlertDialog.Cancel>
-            <Button variant="soft" color="gray">
-              Cancel
-            </Button>
-          </AlertDialog.Cancel>
-          <AlertDialog.Action>
-            <Button
-              variant="solid"
-              color="red"
-              onClick={async () => {
-                await axios.delete(`/api/tasks/${taskId}`);
-                router.push("/tasks");
-              }}
-            >
-              Delete Task
-            </Button>
-          </AlertDialog.Action>
-        </Flex>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
+  const deleteTask = async () => {
+    try {
+      await axios.delete(`/api/tasks/${taskId}`);
+      router.push("/tasks");
+    } catch (error) {
+      console.log(error);
+      setError(true);
+    }
+  };
+
+  return (
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red">Delete Task</Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content maxWidth="450px">
+          <AlertDialog.Title>Confirm deletion</AlertDialog.Title>
+          <AlertDialog.Description size="2">
+            Are you sure you want to delete this task? This action cannot be
+            undone .
+          </AlertDialog.Description>
+
+          <Flex gap="3" mt="4" justify="end">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button variant="solid" color="red" onClick={deleteTask}>
+                Delete Task
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+      <AlertDialog.Root open={error}>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Error</AlertDialog.Title>
+          <AlertDialog.Description>
+            An error occurred while deleting the task. Please try again.
+          </AlertDialog.Description>
+          <Flex gap="3" mt="4" justify="end">
+            <AlertDialog.Cancel>
+              <Button
+                variant="soft"
+                color="gray"
+                onClick={() => setError(false)}
+              >
+                Ok
+              </Button>
+            </AlertDialog.Cancel>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
   );
 };
 
