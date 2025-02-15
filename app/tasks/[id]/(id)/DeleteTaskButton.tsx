6d060b2b362d303e567f75caeb8fb2ded4e730e7
HLockward <HLockward12@gmail.com>
2025-02-14 20:17:16 -0400

    Show a confirmation dialog box

--- app/tasks/[id]/(id)/DeleteTaskButton.tsx
+++ app/tasks/[id]/(id)/DeleteTaskButton.tsx
@@ -1,7 +1,34 @@
-import { Button } from "@radix-ui/themes";
+"use client";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 
 const DeleteTaskButton = ({ taskId }: { taskId: number }) => {
-  return <Button color="red">Delete Task</Button>;
+  return (
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">Delete Task</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content maxWidth="450px">
+        <AlertDialog.Title>Confirm deletion</AlertDialog.Title>
+        <AlertDialog.Description size="2">
+          Are you sure you want to delete this task? This action cannot be
+          undone .
+        </AlertDialog.Description>
+
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="red">
+              Delete Task
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  );
 };
 
 export default DeleteTaskButton;
