bef75f5a2571f3657d4011d16401987f35b0d2be
HLockward <HLockward12@gmail.com>
2025-02-17 23:30:23 -0400

    Fix google provider, now we can change account

--- auth.ts
+++ auth.ts
@@ -5,7 +5,15 @@ import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Google],
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+      authorization: {
+        url: `https://accounts.google.com/o/oauth2/auth/authorize?response_type=code&prompt=login`,
+      },
+    }),
+  ],
   session: {
     strategy: "jwt",
   },
