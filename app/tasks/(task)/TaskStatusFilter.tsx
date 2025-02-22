2dc4cf3309ad4e1a4a4427dff43e1889c58cf8f9
HLockward <HLockward12@gmail.com>
2025-02-22 18:10:06 -0400

    Refactor and fix filtering bugs

--- app/tasks/(task)/TaskStatusFilter.tsx
+++ app/tasks/(task)/TaskStatusFilter.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const Statuses: { label: string; value?: Status | "ALL" }[] = [
   { label: "All", value: "ALL" },
@@ -12,12 +12,21 @@ const Statuses: { label: string; value?: Status | "ALL" }[] = [
 
 const TaskStatusFilter = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const FilterByStatus = (status: string) => {
+    const params = new URLSearchParams();
+    if (status !== "ALL") params.append("status", status);
+    if (searchParams.get("orderBy"))
+      params.append("orderBy", searchParams.get("orderBy")!);
+    const query = params.size ? `?${params.toString()}` : "";
+    router.push(`/tasks${query}`);
+  };
+
   return (
     <Select.Root
-      onValueChange={(status) => {
-        const query = status === "ALL" ? "" : `?status=${status}`;
-        router.push(`/tasks${query}`);
-      }}
+      defaultValue={searchParams.get("status") || "ALL"}
+      onValueChange={(value) => FilterByStatus(value)}
     >
       <Select.Trigger placeholder="Filter by status..." />
       <Select.Content>
